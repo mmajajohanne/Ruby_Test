5b05fe177ed2fe87f007f9772c93cb17627fe64a
Maja Marjamaa <82090456+mmajajohanne@users.noreply.github.com>
2022-11-07 22:16:19 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ def sum_eq_n?(arr, n)
     return true if arr.empty? && n == 0
   
     arr.product(arr).reject { |a,b| a == b }.any? { |a,b| a + b == n }
-end
+end 
 
 #aritmetisk rekke
 def find_missing(sequence)
